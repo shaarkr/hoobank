6bc2204b0a8ae3ffedf522e08cb6798e34259a73
Sharath Kumar <sharathkr@protonmail.com>
2024-01-23 00:25:18 +0530

    feat: add CardDeal section - update Button border radius

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,7 @@
 const Button = ({ styles }) => (
   <button
     type='button'
-    className={`px-6 py-4 font-medium bg-blue-gradient font-poppins text-[18px] text-primary outline-none ${styles}`}
+    className={`px-6 py-4 font-medium bg-blue-gradient font-poppins text-[18px] text-primary outline-none rounded-[10px] ${styles}`}
   >
     Get Started
   </button>
--- src/components/CardDeal.jsx
+++ src/components/CardDeal.jsx
@@ -1,5 +1,27 @@
-const CardDeal = () => {
-  return <div>CardDeal</div>
-}
+import { card } from '../assets'
+import styles, { layout } from '../style'
+import Button from './Button'
+
+const CardDeal = () => (
+  <section id='cards' className={layout.section}>
+    <div className={layout.sectionInfo}>
+      <h2 className={styles.heading2}>
+        Find a better card deal <br className='hidden sm:block' /> in few easy
+        steps.
+      </h2>
+      <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+        Debitis accusamus adipisci error quod enim voluptates iusto illo
+        corrupti numquam commodi delectus aliquam natus nam fuga aperiam. Nobis
+        incidunt explicabo debitis accusamus fugiat quis dignissimos odit itaque
+        nisi a.
+      </p>
+      <Button styles='mt-10' />
+    </div>
+
+    <div className={layout.sectionImg}>
+      <img src={card} alt='card' className='w-full h-full' />
+    </div>
+  </section>
+)
 
 export default CardDeal

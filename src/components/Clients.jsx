e29d40bc9abf9a8d39cfa3947d2739d8d6a6482e
Sharath Kumar <sharathkr@protonmail.com>
2024-01-23 00:50:40 +0530

    feat: add Clients section

--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -1,5 +1,23 @@
-const Clients = () => {
-  return <div>Clients</div>
-}
+import { clients } from '../constants'
+import styles from '../style'
+
+const Clients = () => (
+  <section id='clients' className={`${styles.flexCenter} my-4`}>
+    <div className={`${styles.flexCenter} flex-wrap w-full`}>
+      {clients.map(client => (
+        <div
+          key={client.id}
+          className={`${styles.flexCenter} flex-1 sm:min-w-[192px] min-w-[120px]`}
+        >
+          <img
+            src={client.logo}
+            alt='client'
+            className='object-contain w-[192px] h-[100px]'
+          />
+        </div>
+      ))}
+    </div>
+  </section>
+)
 
 export default Clients

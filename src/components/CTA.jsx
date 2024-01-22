54dc1fee53c91b0ab4a8d6d09c6fadc6613c832f
Sharath Kumar <sharathkr@protonmail.com>
2024-01-23 00:57:56 +0530

    feat: add CTA section

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -1,5 +1,22 @@
-const CTA = () => {
-  return <div>CTA</div>
-}
+import styles from '../style'
+import Button from './Button'
+
+const CTA = () => (
+  <section
+    className={`${styles.flexCenter} ${styles.padding} ${styles.marginY} sm:flex-row flex-col bg-black-gradient-2 rounded-[20px] box-shadow`}
+  >
+    <div className='flex-col flex-1'>
+      <h2 className={styles.heading2}>Let&apos;s try our service now!</h2>
+      <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+        Everything you need to accept credit card payments and grow your
+        business anywhere on the planet.
+      </p>
+    </div>
+
+    <div className={`${styles.flexCenter} sm:ml-10 ml-0 sm:mt-0 mt-10`}>
+      <Button />
+    </div>
+  </section>
+)
 
 export default CTA

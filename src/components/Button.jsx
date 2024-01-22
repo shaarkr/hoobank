3ca7bac1137e97a3977d05bcb78b9813c9088843
Sharath Kumar <sharathkr@protonmail.com>
2024-01-23 00:01:46 +0530

    feat: add BUsiness Features section - add Button component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,5 +1,10 @@
-const Button = () => {
-  return <div>Button</div>
-}
+const Button = ({ styles }) => (
+  <button
+    type='button'
+    className={`px-6 py-4 font-medium bg-blue-gradient font-poppins text-[18px] text-primary outline-none ${styles}`}
+  >
+    Get Started
+  </button>
+)
 
 export default Button
